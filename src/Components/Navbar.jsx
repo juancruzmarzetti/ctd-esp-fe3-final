abd5a5d8e75969118618f002260286c8313105e6
juancruzmarzetti <naujmarz@gmail.com>
2024-06-15 18:19:25 -0300

    step-1-router-links-and-outlet

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -6,9 +7,12 @@ const Navbar = () => {
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <ul>
+        <li><Link to="/home" >Home</Link></li>
+        <li><Link to="/contact" >Contact</Link></li>
+        <li><Link to="/favs" >Favs</Link></li>
+        <li><button>Change theme</button></li>
+      </ul>
     </nav>
   )
 }
