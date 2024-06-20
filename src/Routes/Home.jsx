6eea92fb1b2a65394b5b057bfd28712082a5f94e
juancruzmarzetti <naujmarz@gmail.com>
2024-06-20 15:18:45 -0300

    loading-home

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
 import { useContextGlobal } from '../Components/utils/global.context'
 import { Link } from 'react-router-dom';
@@ -10,8 +10,24 @@ import { Link } from 'react-router-dom';
 const Home = () => {
 
   const {state} = useContextGlobal();
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    if(state.data.length > 0){
+      setLoading(false);
+    }else{
+      setLoading(true);
+    }
+  }, [state.data]);
   
+  if (loading) {
+    return (
+      <div className={`${state.theme} flex flex-col justify-center items-center h-screen`}>
+        <div className="spinner" />
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <main className={`${state.theme} w-screen pb-10` } >
