234233a0ea33e8d4a93f0afc5260e5d20103d4dd
juancruzmarzetti <naujmarz@gmail.com>
2024-06-20 16:02:14 -0300

    form-validations-and-details

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -17,13 +17,13 @@ const Form = ({theme}) => {
   }
 
   const validateEmail = (email) => {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const regex = /^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])$/;
     return regex.test(email);
   };
 
   function handleSubmit(e){
     e.preventDefault();
-    if(user.name.length > 5 && validateEmail(user.email)){
+    if(user.name.trim().length > 5 && validateEmail(user.email)){
       console.log( `Nombre completo: ${user.name} // Email: ${user.email}`);
       showError && setShowError(false);
       setValidation(true);
