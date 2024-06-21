d5a87f551d0e3eea521323f753f7c7e9ddd4ba2e
juancruzmarzetti <naujmarz@gmail.com>
2024-06-21 18:41:02 -0300

    class-to-className

--- src/Components/FormError.jsx
+++ src/Components/FormError.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const FormError = () => {
   return (
     <>
-        <div class="mt-36 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <strong class="font-bold">Información erronea! </strong>
-            <span class="block sm:inline">Por favor verifique su información nuevamente.</span>
+        <div className="mt-36 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong className="font-bold">Información erronea! </strong>
+            <span className="block sm:inline">Por favor verifique su información nuevamente.</span>
         </div>
     </>
   )
--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -4,9 +4,9 @@ export const Message = ({user}) => {
     const {name} = user;
   return (
     <>
-        <div class="mt-36 bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
-            <p class="font-bold">Gracias {name}</p>
-            <p class="text-sm">Te contactaremos cuando antes vía mail</p>
+        <div className="mt-36 bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+            <p className="font-bold">Gracias {name}</p>
+            <p className="text-sm">Te contactaremos cuando antes vía mail</p>
         </div> 
     </>
   )
